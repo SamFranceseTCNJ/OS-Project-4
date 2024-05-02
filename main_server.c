f14e24d2bd96e0a7c6225f1f8d60c1d23e38e81b
Nunziomvd <154708602+Nunziomvd@users.noreply.github.com>
2024-05-02 11:37:29 -0400

    Rooms added but errors with username and sending msgs

--- main_server.c
+++ main_server.c
@@ -8,7 +8,8 @@
 #include <arpa/inet.h>
 #include <pthread.h>
 
-#define PORT_NUM 1004
+#define PORT_NUM 4444
+#define MAX_ROOMS 3
 
 void error(const char *msg)
 {
@@ -24,35 +25,49 @@ typedef struct _USR {
     struct _USR* next;  // for linked list queue
 } USR;
 
-USR *head = NULL;
-USR *tail = NULL;
+typedef struct _Room {
+    int room_number;
+    USR* head;
+    USR* tail;
+    struct _Room* next;
+} Room;
 
-void add_tail(int newclisockfd, char* addr, int color, char* username)
+Room *rooms[MAX_ROOMS] = {NULL};
+
+Room* find_room(int room_number) {
+    for (int i = 0; i < MAX_ROOMS; ++i) {
+        if (rooms[i] != NULL && rooms[i]->room_number == room_number)
+            return rooms[i];
+    }
+    return NULL;
+}
+
+void add_tail(Room* room, int newclisockfd, char* addr, int color, char* username)
 {
-    if (head == NULL) {
-        head = (USR*) malloc(sizeof(USR));
-        head->clisockfd = newclisockfd;
-        head->ipaddr = addr;
-        head->color = color;
-        strcpy(head->username, username);
-        head->next = NULL;
-        tail = head;
+    if (room->head == NULL) {
+        room->head = (USR*) malloc(sizeof(USR));
+        room->head->clisockfd = newclisockfd;
+        room->head->ipaddr = addr;
+        room->head->color = color;
+        strcpy(room->head->username, username);
+        room->head->next = NULL;
+        room->tail = room->head;
     } else {
-        tail->next = (USR*) malloc(sizeof(USR));
-        tail->next->clisockfd = newclisockfd;
-        tail->next->ipaddr = addr;
-        tail->next->color = color;
-        strcpy(tail->next->username, username);
-        tail->next->next = NULL;
-        tail = tail->next;
+        room->tail->next = (USR*) malloc(sizeof(USR));
+        room->tail->next->clisockfd = newclisockfd;
+        room->tail->next->ipaddr = addr;
+        room->tail->next->color = color;
+        strcpy(room->tail->next->username, username);
+        room->tail->next->next = NULL;
+        room->tail = room->tail->next;
     }
 }
 
-int deleteClient(int sockfd) {
-    USR* cur = head;
+int deleteClient(Room* room, int sockfd) {
+    USR* cur = room->head;
     USR* prev;
     if(cur != NULL && cur->clisockfd == sockfd) {
-        head = cur->next;
+        room->head = cur->next;
         printf("%s disconnected from the server\n", cur->username);
         free(cur);
         return 1;
@@ -68,41 +83,19 @@ int deleteClient(int sockfd) {
     return 1;
 }
 
-void printClients() {
-    USR* cur = head;
-    printf("Connected Clients: \n");
+void printClients(Room* room) {
+    USR* cur = room->head;
+    printf("Connected Clients in Room %d: \n", room->room_number);
     while(cur != NULL) {
         printf("    %s\n", cur->username);
         cur = cur->next;
     }
 }
 
-int getColor(int fd) {
-    USR* cur = head;
-    while(cur != NULL) {
-        if(cur->clisockfd == fd) {
-            return cur->color;
-        }
-        cur = cur->next;
-    }
-    return 0;
-}
-
-void setName(int fd, char* name) {
-    USR* cur = head;
-    while(cur != NULL) {
-        if(cur->clisockfd == fd) {
-            strcpy(cur->username, name);
-        }
-        cur = cur->next;
-    }
-}
-
-void broadcast(int fromfd, char* username, char* message)
+void broadcast(Room* room, int fromfd, char* username, char* message)
 {
     // traverse through all connected clients
-    USR* cur = head;
-    int color = getColor(fromfd);
+    USR* cur = room->head;
     while (cur != NULL) {
         // check if cur is not the one who sent the message
         if (cur->clisockfd != fromfd) {
@@ -110,7 +103,7 @@ void broadcast(int fromfd, char* username, char* message)
 
             // prepare message
             memset(buffer, 0, 512);
-            sprintf(buffer, "\x1b[%dm[%s]:%s\x1b[0m", color, username, message);
+            sprintf(buffer, "\x1b[%dm[Room %d][%s]: %s\x1b[0m", cur->color, room->room_number, username, message);
             int nmsg = strlen(buffer);
 
             // send!
@@ -124,6 +117,7 @@ void broadcast(int fromfd, char* username, char* message)
 
 typedef struct _ThreadArgs {
     int clisockfd;
+    int room_number;
 } ThreadArgs;
 
 void* thread_main(void* args)
@@ -132,8 +126,17 @@ void* thread_main(void* args)
     pthread_detach(pthread_self());
 
     // get socket descriptor from argument
-    int clisockfd = ((ThreadArgs*) args)->clisockfd;
+    ThreadArgs* targs = (ThreadArgs*) args;
+    int clisockfd = targs->clisockfd;
+    int room_number = targs->room_number;
     free(args);
+
+    Room* room = find_room(room_number);
+    if (room == NULL) {
+        printf("Room %d not found\n", room_number);
+        close(clisockfd);
+        return NULL;
+    }
 
     //-------------------------------
     // Now, we receive/send messages
@@ -145,11 +148,10 @@ void* thread_main(void* args)
     nrcv = recv(clisockfd, username, 20, 0);
     if (nrcv < 0) error("ERROR recv() failed");
     username[nrcv] = '\0';
-    setName(clisockfd, username);
 
     // we send the user name to everyone
-    printf("%s (IP-address-of-%s) joined the chat room!\n", username, username);
-    broadcast(clisockfd, username, "joined the chat room!");
+    printf("%s (IP-address-of-%s) joined Room %d!\n", username, username, room_number);
+    broadcast(room, clisockfd, username, "joined the chat room!");
 
     while (1) {
         // receive message from client
@@ -160,13 +162,13 @@ void* thread_main(void* args)
         if (nrcv == 0) break; // client disconnected
 
         // we send the message to everyone except the sender
-        broadcast(clisockfd, username, buffer);
+        broadcast(room, clisockfd, username, buffer);
     }
-    broadcast(clisockfd, username, " disconnected.");
-    if(deleteClient(clisockfd) == 0) {
+
+    if(deleteClient(room, clisockfd) == 0) {
         printf("could not disconnect that socket\n");
     }
-    printClients();
+    printClients(room);
     close(clisockfd);
     //-------------------------------
 
@@ -183,7 +185,6 @@ int main(int argc, char *argv[])
     memset((char*) &serv_addr, 0, sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_addr.s_addr = INADDR_ANY;    
-    //serv_addr.sin_addr.s_addr = inet_addr("192.168.1.171");    
     serv_addr.sin_port = htons(PORT_NUM);
 
     int status = bind(sockfd, 
@@ -200,18 +201,50 @@ int main(int argc, char *argv[])
         if (newsockfd < 0) error("ERROR on accept");
 
         printf("Connected: %s\n", inet_ntoa(cli_addr.sin_addr));
-        add_tail(newsockfd, inet_ntoa(cli_addr.sin_addr), (rand() % 8) + 30, "unknown"); // add this new client to the client list
-        printClients();
 
-        // send a message asking for user name
-        char msg[] = "Type your user name: ";
-        send(newsockfd, msg, sizeof(msg), 0);
+        char buffer[256];
+        int n = recv(newsockfd, buffer, 255, 0);
+        if (n < 0) error("ERROR recv() failed");
+
+        if (strncmp(buffer, "new", 3) == 0) {
+            int room_number = -1;
+            for (int i = 0; i < MAX_ROOMS; ++i) {
+                if (rooms[i] == NULL) {
+                    room_number = i + 1;
+                    rooms[i] = (Room*) malloc(sizeof(Room));
+                    rooms[i]->room_number = room_number;
+                    rooms[i]->head = NULL;
+                    rooms[i]->tail = NULL;
+                    break;
+                }
+            }
+            if (room_number == -1) {
+                send(newsockfd, "Maximum number of rooms reached!", sizeof("Maximum number of rooms reached!"), 0);
+                close(newsockfd);
+                continue;
+            }
+            printf("New room created: %d\n", room_number);
+            sprintf(buffer, "Connected to %s with new room number %d\nPlease enter the message: ", inet_ntoa(cli_addr.sin_addr), room_number);
+            send(newsockfd, buffer, sizeof(buffer), 0);
+        } else {
+            int room_number = atoi(buffer);
+            Room* room = find_room(room_number);
+            if (room == NULL) {
+                send(newsockfd, "Room does not exist!", sizeof("Room does not exist!"), 0);
+                close(newsockfd);
+                continue;
+            }
+            printf("Client joined room %d\n", room_number);
+            sprintf(buffer, "Connected to %s in room %d\nPlease enter the message: ", inet_ntoa(cli_addr.sin_addr), room_number);
+            send(newsockfd, buffer, sizeof(buffer), 0);
+        }
 
         // prepare ThreadArgs structure to pass client socket
         ThreadArgs* args = (ThreadArgs*) malloc(sizeof(ThreadArgs));
         if (args == NULL) error("ERROR creating thread argument");
         
         args->clisockfd = newsockfd;
+        args->room_number = atoi(buffer);
 
         pthread_t tid;
         if (pthread_create(&tid, NULL, thread_main, (void*) args) != 0) error("ERROR creating a new thread");
